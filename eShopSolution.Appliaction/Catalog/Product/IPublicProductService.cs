292444806e2d066a198cff8dcbb3408d22586e37
xuanloc <xuanloc.hcm2018@gmail.com>
2021-03-16 23:24:56 +0700

    Edit , Update , GetAllCategory

--- eShopSolution.Appliaction/Catalog/Product/IPublicProductService.cs
+++ eShopSolution.Appliaction/Catalog/Product/IPublicProductService.cs
@@ -1,14 +1,17 @@
 ﻿using eShopSolution.Appliaction.Catalog.Product.Dtos;
-using eShopSolution.Appliaction.Catalog.Product.Dtos.Manage;
+using eShopSolution.Appliaction.Catalog.Product.Dtos.Public;
 using eShopSolution.Appliaction.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace eShopSolution.Appliaction.Catalog.Product
 {
     public interface IPublicProductService
     {
-        PagedResult<ProductViewModel> GetAllCategoryId (GetProductPagingRequest request);
+        Task<PagedResult<ProductViewModel>> GetAllCategoryId (GetProductPagingRequest request);
+
+        Task<List<ProductViewModel>> GetAll();
     }
 }
--- eShopSolution.Appliaction/Catalog/Product/ManageProductService.cs
+++ eShopSolution.Appliaction/Catalog/Product/ManageProductService.cs
@@ -67,13 +67,20 @@ namespace eShopSolution.Appliaction.Catalog.Product
 
         public async Task<int> edit(ProductEditRequest request)
         {
-            throw new NotImplementedException();
+            var product = await _context.Products.FindAsync(request.Id);
+            var productTranslations =await _context.ProductTranslations.FirstOrDefaultAsync(x => x.ProductId == request.Id && x.LanguageId == request.LanguageId);
+            if (product == null || productTranslations ==null) throw new EShopException($"Cannot Find A Product With Id : {request.Id}");
+
+            productTranslations.Name = request.Name;
+            productTranslations.SeoAlias = request.SeoAlias;
+            productTranslations.SeoDescription = request.SeoDescription;
+            productTranslations.SeoTitle = request.SeoTitle;
+            productTranslations.Description = request.Description;
+            productTranslations.Details = request.Details;
+            return await _context.SaveChangesAsync();
         }
 
-        public async Task<List<ProductViewModel>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+      
 
         public async Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request)
         {
@@ -125,12 +132,18 @@ namespace eShopSolution.Appliaction.Catalog.Product
 
         public async Task<bool> UpdatePrice(int productId, decimal newPrice)
         {
-            throw new NotImplementedException();
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null ) throw new EShopException($"Cannot Find A Product With Id : {productId}");
+            product.Price = newPrice;
+            return await _context.SaveChangesAsync() > 0; 
         }
 
         public async Task<bool> UpdateStock(int productId, int addQuantity)
         {
-            throw new NotImplementedException();
+            var product = await _context.Products.FindAsync(productId);
+            if (product == null) throw new EShopException($"Cannot Find A Product With Id : {productId}");
+            product.Stock += addQuantity;
+            return await _context.SaveChangesAsync() > 0;
         }
     }
 }
