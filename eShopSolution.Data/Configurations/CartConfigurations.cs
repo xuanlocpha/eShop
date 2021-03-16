435eda9e66e6114e3f240db367762774719a4ef6
xuanloc <xuanloc.hcm2018@gmail.com>
2021-03-16 11:47:27 +0700

    Cấu hình Fluent và Migration Database

---
+++ eShopSolution.Data/Configurations/CartConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    public class CartConfigurations : IEntityTypeConfiguration<Cart>
+    {
+        public void Configure(EntityTypeBuilder<Cart> builder)
+        {
+            builder.ToTable("Carts");
+
+            builder.HasKey(x => x.Id);
+
+            builder.HasOne(x => x.Product).WithMany(x => x.Carts).HasForeignKey(x => x.ProductId);
+        }
+    }
+}
---
+++ eShopSolution.Data/EF/EShopDBContext.cs
@@ -0,0 +1,54 @@
+﻿using eShopSolution.Data.Configurations;
+using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class EShopDBContext : DbContext
+    {
+        public EShopDBContext( DbContextOptions options) : base(options)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        
+
+            modelBuilder.ApplyConfiguration(new AppConfigConfigurations());
+            modelBuilder.ApplyConfiguration(new CategoryConfigurations());
+            modelBuilder.ApplyConfiguration(new CategoryTranslationConfigurations());
+            modelBuilder.ApplyConfiguration(new ContactConfigurations());
+            modelBuilder.ApplyConfiguration(new CartConfigurations());
+
+            modelBuilder.ApplyConfiguration(new LanguageConfigurations());
+            modelBuilder.ApplyConfiguration(new OrderConfigurations());
+            modelBuilder.ApplyConfiguration(new OrderDetailConfigurations());
+            modelBuilder.ApplyConfiguration(new ProductConfigurations());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfigurations());
+            modelBuilder.ApplyConfiguration(new ProductTranslationConfigurations());
+            modelBuilder.ApplyConfiguration(new PromotionConfigurations());
+            modelBuilder.ApplyConfiguration(new TransactionConfigurations());
+
+            //base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<AppConfig> AppConfigs { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}
